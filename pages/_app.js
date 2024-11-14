255d634655a721c0864a800d86c39b51ded3d2d4
WhatWasHisNameAgain <45083673+whatwashisnameagain@users.noreply.github.com>
2024-11-14 20:32:26 +0000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,26 @@
 import Head from 'next/head';
-import Script from 'next/script'
+import { useEffect } from 'react';
 import '@styles/home.css'
 import '@styles/contact.css'
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Hotjar Tracking Code
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:process.env.NEXT_PUBLIC_HOTJAR_ID, hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  }, []);
   return (
   <>
     <Head>
         <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap" rel="stylesheet" />
-        <Script>{`
-          (function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); 
-        `}</Script>
     </Head>
     <Component {...pageProps} />
   </>)
