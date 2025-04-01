499a02c49e048d8fe8706610ec5dc693865a1f33
Ben Remy <45083673+benremy@users.noreply.github.com>
2025-04-01 15:08:38 +0000

    initialize reply button component

---
+++ components/ReplyButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@/componenents/Button'
+
+export default function ReplyButton() {
+    return (
+        <Button 
+            onClick={undefined}
+            value="Reply"
+        />
+    )
+}
